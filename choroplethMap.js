1ac68eab2f5aefa4b851f64eb79368dff1e70657
Jonathan Howard <2773262@gmail.com>
2021-02-17 21:08:00 -0500

    Add data-fips and data-education attributes

--- choroplethMap.js
+++ choroplethMap.js
@@ -1,9 +1,30 @@
 choroplethMap().catch(console.error);
 
 async function choroplethMap() {
+  /**
+   * topojson data
+   * @type {{
+   *   objects: {
+   *     counties: object,
+   *   },
+   * }}
+   */
   const us = await d3.json(
-    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json",
   );
+
+  /**
+   * education data
+   * @type {[{
+   *   bachelorsOrHigher: number,
+   *   fips: number,
+   * }]}
+   */
+  const education = await d3.json(
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json",
+  );
+
+  const dataset = new Map(education.map(d => [d.fips, d.bachelorsOrHigher]));
 
   const svg = d3.select("#root").append("svg")
     .attr("width", window.innerWidth)
@@ -15,6 +36,8 @@ async function choroplethMap() {
     .data(topojson.feature(us, us.objects.counties).features)
     .join("path")
     .attr("class", "county")
+    .attr("data-fips", d => d.id)
+    .attr("data-education", d => dataset.get(d.id))
     .attr("fill", "none")
     .attr("stroke", "#333333")
     .attr("stroke-width", "0.2")
