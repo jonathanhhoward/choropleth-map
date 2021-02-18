0229f097b9b1787208f27112e5fc14e23d545c84
Jonathan Howard <2773262@gmail.com>
2021-02-17 20:10:53 -0500

    Initial commit

---
+++ choroplethMap.js
@@ -0,0 +1,21 @@
+choroplethMap().catch(console.error);
+
+async function choroplethMap() {
+  const us = await d3.json(
+    "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+  );
+
+  const svg = d3.select("#root").append("svg")
+    .attr("width", window.innerWidth)
+    .attr("height", window.innerHeight);
+
+  const map = svg.append("g");
+
+  map.selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .join("path")
+    .attr("fill", "none")
+    .attr("stroke", "#333333")
+    .attr("stroke-width", "0.2")
+    .attr("d", d3.geoPath());
+}
