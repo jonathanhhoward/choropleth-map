70c0b448c4332d370631d15f5a798964b4a27d3c
Jonathan Howard <2773262@gmail.com>
2021-02-19 11:42:54 -0500

    Add legend

--- choroplethMap.js
+++ choroplethMap.js
@@ -28,14 +28,8 @@ async function choroplethMap() {
   const width = 975;
   const height = 610;
   const margin = { top: 100, right: 0, bottom: 0, left: 0 };
-  const dataset = new Map(education.map(d => [d.fips, d.bachelorsOrHigher]));
   const educationDomain = d3.extent(education.map(d => d.bachelorsOrHigher));
   const colorScale = d3.scaleQuantize(educationDomain, d3.schemeBuGn[9]);
-  const path = d3.geoPath();
-  const interiorBorders = (a, b) => (a !== b);
-  const countyStrokeWidth = 0.1;
-  const stateStrokeWidth = 0.25;
-  const stroke = "#555555";
 
   const svg = d3.select("#root").append("svg")
     .attr("width", width)
@@ -61,6 +55,48 @@ async function choroplethMap() {
     .attr("x", width * 0.5)
     .attr("y", d => d.y)
     .text(d => d.text);
+
+  const legendWidth = 300;
+  const legendHeight = 8;
+
+  const legend = svg.append("g")
+    .attr("id", "legend")
+    .attr(
+      "transform",
+      `translate(${(width / 2) - (legendWidth / 2)}, ${margin.top})`,
+    );
+
+  const legendData = colorScale.thresholds();
+
+  const legendScale = d3.scaleBand()
+    .domain(legendData)
+    .range([0, legendWidth]);
+
+  const legendAxis = d3.axisBottom(legendScale)
+    .tickFormat(d => `${d3.format(".1f")(d)}%`)
+    .tickSizeOuter(0);
+
+  legend.append("g")
+    .attr("transform", `translate(0, ${legendHeight})`)
+    .call(legendAxis)
+    .call(g => g.select(".domain").remove());
+
+  legend.selectAll("rect")
+    .data(legendData)
+    .enter()
+    .append("rect")
+    .attr("x", d => legendScale(d))
+    .attr("y", 0)
+    .attr("width", legendScale.bandwidth())
+    .attr("height", legendHeight)
+    .attr("fill", d => colorScale(d));
+
+  const dataset = new Map(education.map(d => [d.fips, d.bachelorsOrHigher]));
+  const path = d3.geoPath();
+  const interiorBorders = (a, b) => (a !== b);
+  const countyStrokeWidth = 0.1;
+  const stateStrokeWidth = 0.25;
+  const stroke = "#555555";
 
   const map = svg.append("g")
     .attr("transform", `translate(0, ${margin.top})`);
