588b7141c2ee7436be6363f365d5d5e9694f4119
Jonathan Howard <2773262@gmail.com>
2021-02-22 10:08:17 -0500

    Add tooltip

--- choroplethMap.js
+++ choroplethMap.js
@@ -1,8 +1,6 @@
 import { Runtime } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
 import d3ColorLegend from "https://api.observablehq.com/@d3/color-legend.js?v=3";
 
-const colorLegendNotebook = new Runtime().module(d3ColorLegend);
-
 choroplethMap().catch(console.error);
 
 async function choroplethMap() {
@@ -22,6 +20,7 @@ async function choroplethMap() {
   /**
    * education data
    * @type {[{
+   *   area_name: string,
    *   bachelorsOrHigher: number,
    *   fips: number,
    * }]}
@@ -36,7 +35,9 @@ async function choroplethMap() {
   const educationDomain = d3.extent(education.map(d => d.bachelorsOrHigher));
   const color = d3.scaleQuantize(educationDomain, d3.schemeBuGn[9]);
 
-  const svg = d3.select("#root").append("svg")
+  const root = d3.select("#root");
+
+  const svg = root.append("svg")
     .attr("width", width)
     .attr("height", height + margin.top)
     .attr("class", "centered");
@@ -61,8 +62,8 @@ async function choroplethMap() {
     .attr("y", d => d.y)
     .text(d => d.text);
 
+  const colorLegendNotebook = new Runtime().module(d3ColorLegend);
   const legend = await colorLegendNotebook.value("legend");
-
   const legendWidth = 300;
 
   svg.append("g")
@@ -80,11 +81,12 @@ async function choroplethMap() {
       tickFormat: d3.format(".0f"),
     }));
 
-  const dataset = new Map(education.map(d => [d.fips, d.bachelorsOrHigher]));
+  const educationLevel = new Map(education.map(d => [
+    d.fips,
+    d.bachelorsOrHigher,
+  ]));
   const path = d3.geoPath();
   const interiorBorders = (a, b) => (a !== b);
-  const countyStrokeWidth = 0.1;
-  const stateStrokeWidth = 0.25;
   const stroke = "#555555";
 
   const map = svg.append("g")
@@ -95,21 +97,40 @@ async function choroplethMap() {
     .join("path")
     .attr("class", "county")
     .attr("data-fips", d => d.id)
-    .attr("data-education", d => dataset.get(d.id))
-    .attr("fill", d => color(dataset.get(d.id)))
-    .attr("d", path);
+    .attr("data-education", d => educationLevel.get(d.id))
+    .attr("fill", d => color(educationLevel.get(d.id)))
+    .attr("d", path)
+    .on("mouseover", showTooltip)
+    .on("mouseout", hideTooltip);
 
   map.append("path")
     .datum(topojson.mesh(us, us.objects.counties, interiorBorders))
     .attr("fill", "none")
     .attr("stroke", stroke)
-    .attr("stroke-width", countyStrokeWidth)
+    .attr("stroke-width", 0.1)
     .attr("d", path);
 
   map.append("path")
     .datum(topojson.mesh(us, us.objects.states, interiorBorders))
     .attr("fill", "none")
     .attr("stroke", stroke)
-    .attr("stroke-width", stateStrokeWidth)
+    .attr("stroke-width", 0.25)
     .attr("d", path);
+
+  const countyName = new Map(education.map(d => [d.fips, d.area_name]));
+
+  const tooltip = root.append("div")
+    .attr("id", "tooltip");
+
+  function showTooltip(event, data) {
+    tooltip.attr("data-education", educationLevel.get(data.id))
+      .html(`${countyName.get(data.id)}<br>${(educationLevel.get(data.id))}%`)
+      .style("left", `${(event.pageX + 20)}px`)
+      .style("top", `${(event.pageY - 40)}px`)
+      .style("display", "block");
+  }
+
+  function hideTooltip() {
+    tooltip.style("display", "none");
+  }
 }
